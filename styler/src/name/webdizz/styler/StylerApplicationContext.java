55eb6eb738bfa7811f57965d16848ad03f7ca158
webdizz <webdizz@gmail.com>
2010-05-15 16:09:49 +0300

    Test with formatting source code was implemented

--- styler/src/name/webdizz/styler/StylerApplicationContext.java
+++ styler/src/name/webdizz/styler/StylerApplicationContext.java
@@ -11,73 +11,104 @@ import org.osgi.framework.Bundle;
 
 /**
  * @author webdizz
- *
+ * 
  */
 public class StylerApplicationContext implements IApplicationContext {
 
-	/* (non-Javadoc)
-	 * @see org.eclipse.equinox.app.IApplicationContext#applicationRunning()
-	 */
-	public void applicationRunning() {
-		// TODO Auto-generated method stub
+    public static final String ARG_CONFIG = "-config";
 
-	}
+    public static final String ARG_HELP = "-help";
 
-	/* (non-Javadoc)
-	 * @see org.eclipse.equinox.app.IApplicationContext#getArguments()
-	 */
-	public Map<String, String[]> getArguments() {
-		Map<String, String[]> arguments = new HashMap<String, String[]>();
-		arguments.put(IApplicationContext.APPLICATION_ARGS, new String[]{});
-		return arguments;
-	}
+    public static final String ARG_QUIET = "-quiet";
 
-	/* (non-Javadoc)
-	 * @see org.eclipse.equinox.app.IApplicationContext#getBrandingApplication()
-	 */
-	public String getBrandingApplication() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public static final String ARG_VERBOSE = "-verbose";
+    
+    public static final String PDE_LAUNCH = "-pdelaunch";
 
-	/* (non-Javadoc)
-	 * @see org.eclipse.equinox.app.IApplicationContext#getBrandingBundle()
-	 */
-	public Bundle getBrandingBundle() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    private Map<String, String[]> arguments = new HashMap<String, String[]>();
 
-	/* (non-Javadoc)
-	 * @see org.eclipse.equinox.app.IApplicationContext#getBrandingDescription()
-	 */
-	public String getBrandingDescription() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public StylerApplicationContext(final Map<String, String[]> arguments) {
+	super();
+	this.arguments = arguments;
+    }
 
-	/* (non-Javadoc)
-	 * @see org.eclipse.equinox.app.IApplicationContext#getBrandingId()
-	 */
-	public String getBrandingId() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.equinox.app.IApplicationContext#applicationRunning()
+     */
+    public void applicationRunning() {
+    }
 
-	/* (non-Javadoc)
-	 * @see org.eclipse.equinox.app.IApplicationContext#getBrandingName()
-	 */
-	public String getBrandingName() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.equinox.app.IApplicationContext#getArguments()
+     */
+    public Map<String, String[]> getArguments() {
+	return arguments;
+    }
 
-	/* (non-Javadoc)
-	 * @see org.eclipse.equinox.app.IApplicationContext#getBrandingProperty(java.lang.String)
-	 */
-	public String getBrandingProperty(String arg0) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.equinox.app.IApplicationContext#getBrandingApplication()
+     */
+    public String getBrandingApplication() {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.equinox.app.IApplicationContext#getBrandingBundle()
+     */
+    public Bundle getBrandingBundle() {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.equinox.app.IApplicationContext#getBrandingDescription()
+     */
+    public String getBrandingDescription() {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.equinox.app.IApplicationContext#getBrandingId()
+     */
+    public String getBrandingId() {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.equinox.app.IApplicationContext#getBrandingName()
+     */
+    public String getBrandingName() {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * org.eclipse.equinox.app.IApplicationContext#getBrandingProperty(java.
+     * lang.String)
+     */
+    public String getBrandingProperty(String arg0) {
+	// TODO Auto-generated method stub
+	return null;
+    }
 
 }
--- styler/test/name/webdizz/styler/test/CodeFormatterApplicationTest.java
+++ styler/test/name/webdizz/styler/test/CodeFormatterApplicationTest.java
@@ -2,23 +2,41 @@ package name.webdizz.styler.test;
 
 import static org.junit.Assert.assertNotNull;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import name.webdizz.styler.StylerApplicationContext;
 
 import org.eclipse.equinox.app.IApplication;
 import org.eclipse.equinox.app.IApplicationContext;
 import org.eclipse.jdt.core.formatter.CodeFormatterApplication;
+import org.junit.Before;
 import org.junit.Test;
 
 public class CodeFormatterApplicationTest {
 
-	private IApplicationContext applicationContext = new StylerApplicationContext();
-	
-	@Test
-	public void testStart()  throws Exception {
-		IApplication codeFormatter = new CodeFormatterApplication();
-		assertNotNull(codeFormatter);
-		Object result = codeFormatter.start(applicationContext);
-		assertNotNull(result);
-	}
+    private static Map<String, String[]> arguments = new HashMap<String, String[]>();
+    
+    private static IApplicationContext applicationContext;
+
+    @Before
+    public void beforeTest() {
+	String[] args = new String[] {
+		// specify config path
+		StylerApplicationContext.ARG_CONFIG, "../org.eclipse.jdt.core.prefs",
+		// specify source path
+		"fixture"
+	};
+	arguments.put(IApplicationContext.APPLICATION_ARGS, args);
+	applicationContext = new StylerApplicationContext(arguments);
+    }
+
+    @Test
+    public void testStart() throws Exception {
+	IApplication codeFormatter = new CodeFormatterApplication();
+	assertNotNull(codeFormatter);
+	Object result = codeFormatter.start(applicationContext);
+	assertNotNull(result);
+    }
 
 }
