5ae2b3a75cb52bca11506beaf69d401bfe25ed00
webdizz <webdizz@gmail.com>
2010-09-02 13:21:02 +0300

    Resolving formatter lib was changed

--- jstyler/lib/jstyler.rb
+++ jstyler/lib/jstyler.rb
@@ -11,7 +11,9 @@ module Jstyler
   JAVA_LIBS = File.expand_path(File.dirname(__FILE__)+'/../javalibs')
   
   # resolve formatter lib from available libs 
-  FORMATTER_LIB = Dir.glob(JAVA_LIBS+'/*.jar').select{|item|item.include?('formatter')}.first
+  def FORMATTER_LIB
+  	Dir.glob(JAVA_LIBS+'/*.jar').select{|item|item.include?('formatter')}.first
+  end
   
 end
 
