e70187a7403dc59ab81cfcf0034e4f98c15ba015
webdizz <webdizz@gmail.com>
2010-09-02 11:41:08 +0300

    Dynamic resolving path to formatter lib was added

--- jstyler/lib/jstyler.rb
+++ jstyler/lib/jstyler.rb
@@ -2,13 +2,16 @@ $:.unshift(File.dirname(__FILE__)) unless
 $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
 module Jstyler
-  VERSION = '0.0.4'
+  VERSION = '0.0.5'
   
+  # current gem directory
   GEM_DIRECTORY = File.expand_path(File.dirname(__FILE__)+'/../')
   
-  FORMATTER_LIB = 'formatter-0.0.2.jar'
-  
+  # path to java libs   
   JAVA_LIBS = File.expand_path(File.dirname(__FILE__)+'/../javalibs')
+  
+  # resolve formatter lib from available libs 
+  FORMATTER_LIB = Dir.glob(JAVA_LIBS+'/*.jar').select{|item|item.include?('formatter')}.first
   
 end
 
--- jstyler/spec/jstyler_spec.rb
+++ jstyler/spec/jstyler_spec.rb
@@ -1,11 +1,11 @@
 require File.dirname(__FILE__) + '/spec_helper.rb'
 
-# Time to add your specs!
-# http://rspec.info/
-describe "Place your specs here" do
+include Jstyler
+
+describe Jstyler, "when is being initialized" do
   
-  it "find this spec in spec directory" do
-    # violated "Be sure to write your specs"
+  it "should resolve formatter lib within javalibs folder" do
+    FORMATTER_LIB.should include 'formatter'
   end
   
 end
